ace46511e2ac48774cc256538f77d3ec2a0c2d82
masterjiyuhang <52491970+masterjiyuhang@users.noreply.github.com>
2023-06-30 03:44:04 +0000

    Deploying to gh-pages from @ masterjiyuhang/vite-vue2-clean@0ced95181c08e63758da4ea5fdcb4ab801370dc9 🚀

---
+++ static/js/config.js
@@ -0,0 +1,5 @@
+window.BASE_INFO = {
+  // VUE_APP_BASE_API: "https://api-sit.jctrans.com",
+  // VUE_APP_BASE_API: "https://api-dev2.jctrans.com",
+  VUE_APP_BASE_API: 'https://cloudapi.jctrans.com',
+};
