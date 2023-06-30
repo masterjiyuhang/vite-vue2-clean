4562c8083268e0ea8d55ebaf213ce6d9945bc953
erhang <syjzjyh@163.com>
2023-06-30 11:49:58 +0800

    feat: 🚀 更新router mode

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ Vue.use(VueRouter);
 const routes = [...demoRouters];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: import.meta.env.MODE !== 'production' ? 'history' : 'hash',
   base: import.meta.env.BASE_URL,
   routes,
   scrollBehavior() {
